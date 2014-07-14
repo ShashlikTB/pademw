ce44995a94ea1401266b8183632182d9bf36f01d
Thomas Anderson <hurt@localhost.localdomain>
2014-07-13 22:51:23 -0400

    Taking things in a different direction using boost::asio, unfortunately this version won't work, I have to take a different approach.

--- include/pademw.h
+++ include/pademw.h
@@ -1,21 +1,6 @@
 #ifndef _PADEMW_H
 #define _PADEMW_H
 
-#include <event2/event.h>
-#include <event2/listener.h>
-#include <event2/bufferevent.h>
-#include <event2/buffer.h>
-
-//socket related stuff 
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <strings.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
 
 #include <TString.h>
 #include <tuple>
@@ -23,61 +8,48 @@
 //include boost asio library for socket handling and boost array 
 
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
 
-
-
+using std::shared_ptr; 
 using boost::asio::ip::udp; 
 
 static const char *menu = "You have the following options:\n (A)rm the trigger\n (D)isarm the Trigger\n (T)rigger Controller\n (R)ead Data\n (S)tatus\n (C)lear\n (Q)uit\n";   
 
 
-struct manager_data { 
-  struct event_base *base; 
-  struct event *arm; 
-  struct event *disarm; 
-  struct event *trigger; 
-  struct event *read; 
-  struct event *clear; 
-  struct event *status; 
-  int tcpsock; 
-  int udpsock; 
-}; 
-
-
-
 
 class padeListener { 
  boost::asio::io_service& _io_service; 
- udp::socket _listener;   
+ shared_ptr<udp::socket> _listener;   
+ std::array<char, 1024> recvBuffer; 
+ std::array<char, 128> sndBuffer; 
  
  public:
   //  padeListener(); 
- padeListener(boost::asio::io_service &ioservice) : _io_service(ioservice), _listener(ioservice) {}; 
- padeListener(boost::asio::io_service &ioservice, unsigned int port) : _io_service(ioservice), _listener(ioservice, udp::endpoint(udp::v4(), port)) {}; 
- padeListener(const padeListener &listener); 
+ padeListener(boost::asio::io_service &ioservice) : _io_service(ioservice), _listener(shared_ptr<udp::socket>(new udp::socket(ioservice))) { 
+   boost::asio::socket_base::reuse_address option(true); 
+   _listener->set_option(option); 
+ }
+ padeListener(boost::asio::io_service &ioservice, unsigned int port) : _io_service(ioservice), _listener(shared_ptr<udp::socket>(new udp::socket(ioservice, udp::endpoint(udp::v4(), port)))) {
+   boost::asio::socket_base::reuse_address option(true); 
+   _listener->set_option(option); 
+ }
+ padeListener(const padeListener &listener) : _io_service(listener._io_service), _listener(listener._listener) {}; 
+
+ ~padeListener(); 
+ void operator()(); 
+ bool simplemanagementLoop(); 
   bool sendMessage(const TString &msg); 
-  
+  bool Running(); 
   void Stop(); 
   std::array<char, 1024> _recv; 
   udp::endpoint _remote; 
 };
 
-std::tuple<bool, padeListener *> setupListener(const TString &addr, unsigned int port);
+std::tuple<bool, padeListener *> setupListener(const TString &addr, unsigned int port, boost::asio::io_service &io_service);
   
 
 
 
-class padeEventLoop { 
-  
-  padeListener *_listener; 
-
- public:
-  padeEventLoop() { _listener = NULL; }; 
-    
- padeEventLoop(padeListener *listen) : _listener(listen) {}; 
-  padeEventLoop(const padeEventLoop &pade); 
-
-}; 
 
 
 
--- src/pademw.cpp
+++ src/pademw.cpp
@@ -1,72 +1,108 @@
 #include <pademw.h>
 
 
-
 void padeListener::Stop() { 
-  _listener.close(); 
-  
+
+  try {
+    _io_service.stop(); 
+    //    _listener.shutdown(boost::asio::ip::udp::socket::shutdown_receive); 
+  }
+  catch (std::exception& e) { 
+    std::cerr << e.what() << std::endl; 
+  }
+
 
 }
 
-std::tuple<bool, padeListener *> setupListener(const TString &addr, unsigned int port) { 
-  padeListener *listen = NULL; 
-  boost::asio::io_service io_service; 
-  listen = new padeListener(io_service, port); 
 
+padeListener::~padeListener() { 
+  boost::system::error_code error; 
+  std::cout << "Shutting Down listener!" << std::endl; 
+  _listener->shutdown(boost::asio::ip::udp::socket::shutdown_both, error); 
+  std::cout << "Problem shutting down listener: " << error.value() << std::endl; 
+  std::cout << "Problem shutting down listener: " << error.category().name() << std::endl; 
+  /*try { 
+        _listener.close(error); 
+    std::cout << "Problem closing listener: " << error.value() << std::endl; 
+    std::cout << "Problem closing listener: " << error.category().name() << std::endl;
+    std::cout << "Finished destroying listener!" << std::endl; 
+  }
+  catch (std::exception &e) { 
+    std::cout << "Failed in destroying padelistener." << std::endl; 
+    std::cerr << e.what() << std::endl; 
+    }*/
+
+}
+
+bool padeListener::Running() { 
+  return _listener->is_open(); 
+}
+
+bool padeListener::simplemanagementLoop() { 
+
+  std::cout << "Starting management loop" << std::endl; 
+  boost::system::error_code error; 
+  udp::endpoint remote_endpoint; 
+  for (;;) { 
+
+    _listener->receive_from(boost::asio::buffer(recvBuffer), remote_endpoint, 0, error); 
+    if (error && error != boost::asio::error::message_size)
+      throw boost::system::system_error(error); 
+
+    TString *recvd = new TString(recvBuffer.data()); 
+    recvBuffer = {{ 0 }}; 
+    std::cout << "We received: " << recvd->Data() << std::endl; 
+    if (recvd->Contains("Hello")) {
+      return true; 
+    }
+    if (recvd->Contains("Stop")) { 
+      return false; 
+    }
+    delete recvd; 
+
+  }
+  return false; 
+
+}
+
+void padeListener::operator()() { 
+    _io_service.run(); 
+  std::cout << "Starting management loop" << std::endl; 
+  boost::system::error_code error; 
+
+  for (;;) { 
+    udp::endpoint remote_endpoint;     
+    _listener->receive_from(boost::asio::buffer(recvBuffer), remote_endpoint); 
+
+    if (error && error != boost::asio::error::message_size)
+      throw boost::system::system_error(error); 
+
+    TString *recvd = new TString(recvBuffer.data()); 
+    recvBuffer = {{ 0 }}; 
+    std::cout << "We received: " << recvd->Data() << std::endl; 
+    if (recvd->Contains("Hello")) {
+      return; 
+    }
+    if (recvd->Contains("Stop")) { 
+      return; 
+    }
+    delete recvd; 
+
+  }
+  return; 
+
+}
+
+  
+
+std::tuple<bool, padeListener *> setupListener(const TString &addr, unsigned int port, boost::asio::io_service &io_service) {
+  padeListener *listen = NULL; 
+
+  listen = new padeListener(io_service, port); 
+  
   return std::make_tuple(true, listen); 
 
 }
 
 
-
-padeEventLoop::padeEventLoop(const padeEventLoop &pade) { 
-  _listener = pade._listener; 
-
-}
-
-
-
-/*padeListener::padeListener(const padeListener &listener) { 
-  _listener = listener._listener;
-  _io_service = listener._io_service; 
-  _remote = listener._remote; 
-  _recv = listener._recv; 
-
-  }*/
-
-
-
-
-
-/* {
-  int sock = -1; 
-  padeListener *listen = new padeListener(port); 
-
-  //attempt to setup boost 
-  try { 
-    boost::asio::io_service io_service; 
-    udp::socket socket(io_service, udp::endpoint(udp::v4(), port)); 
-    std::array<char, 500> recv; 
-    udp::endpoint remote; 
-    boost::system::error_code error; 
-    socket.receive_from(boost::asio::buffer(recv), remote, 0, error); 
-    if (error && error != boost::asio::error::message_size)
-      throw boost::system::system_error(error); 
-    std::cout << "Client sent us:" << std::string(recv.data()) << std::endl; 
-    std::string msg = "hello you filthy....\n"; 
-    
-    boost::system::error_code ignored_error; 
-    socket.send_to(boost::asio::buffer(msg), remote, 0, ignored_error); 
-      
-
-  }
-  catch (std::exception& e) { 
-    std::cerr << e.what() << std::endl; 
-    return std::make_tuple(false, listenSocket); 
-  }
-  return std::make_tuple(true, listenSocket); 
-
-} 
-
-*/
 
